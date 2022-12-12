15263b4e1dfca24d8dea0a534f1078b2be8b5ae8
JessicaERO <jerodriguezo@esimurcia.com>
2022-12-12 13:12:14 +0100

    Lógica
    Feedback de Juan Alberto sobre la lógica del código

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -8,31 +8,55 @@ public class PlatformSpawner : MonoBehaviour
     public Transform[] positions; 
     public Vector2Int[] probabilities;
 
-    void Start()
-    {
-        
-    }
+    GameObject platformPos;
 
-    void Update()
-    {
-        
-    }
+    //void Start()
+    //{
+    //    platformPos.transform.position = Vector3.zero;
+    //    Instantiate(platforms, platformPos, transform.rotation);
+    //}
 
-    private void Awake()
-    {
-        
-        for (int i = 0; i < positions.Length; i++)
-        {
-            int typeOfPlatform = Random.Range(0, 100);
-            //si el valor random que se genera es igual a uno que se encuentre dentro de las
-            //probabilidades se genera el tipo de plataforma correspondiente
-            //aqui iría el switch case
-            int hasPlatform = Random.Range(0, 100);
-            if (hasPlatform <= 10)
-            {
-                //Instantiate platforms[/*tipo de plataforma*/] posiciones[i]
-                //si la probabilidad es verdadera se genera el tipo de plataforma 
-            }
-        }
-    }
+    //void Update()
+    //{
+    //    if (playerPosition = PlayerControllerPosition + 10)
+    //    {
+
+    //        for (int i = 0; i < positions.Length; i++)
+    //        {
+    //            platformPos.transform.position = new Vector3(typeOfPlatform, transform.position.y + 10, 0);
+    //            int typeOfPlatform = Random.Range(0, 100);
+    //            //si el valor random que se genera es igual a uno que se encuentre dentro de las
+    //            //probabilidades se genera el tipo de plataforma correspondiente
+    //            //aqui iría el switch case
+
+    //            int hasPlatform = Random.Range(0, 100);
+    //            if (hasPlatform <= 10)
+    //            {
+    //                //Instantiate platforms[/*tipo de plataforma*/] posiciones[i]
+    //                //si la probabilidad es verdadera se genera el tipo de plataforma 
+    //            }
+
+    //            switch (hasPlatform)
+    //            {
+    //                case: hasPlatform < 20
+    //                {
+    //                        //to do:
+    //                        //instanciamos la plataforma 
+    //                        platformPos = Instantiate(platforms, new Vector3(0, , 0), transform.rotation);
+    //                }
+    //                break;
+
+    //                case: hasPlatform > 20 && < 40
+    //                {
+    //                        //to do:
+    //                        //instanciamos la plataforma 
+    //                        Instantiate(platforms, new Vector3(2.5f,  , 0), transform.rotation);
+    //                }
+    //                break;
+    //            }
+
+    //        }
+    //    }
+    //}
+
 }
