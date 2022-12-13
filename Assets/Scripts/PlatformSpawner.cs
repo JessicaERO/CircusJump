649c8b3488529c88f2c7392dfa3848acbecb7c89
JessicaERO <jerodriguezo@esimurcia.com>
2022-12-13 18:30:44 +0100

    Pruebas
    Sigo probando a ver como funciona el switch case qnq

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -9,54 +9,60 @@ public class PlatformSpawner : MonoBehaviour
     public Vector2Int[] probabilities;
 
     GameObject platformPos;
+    GameObject playerPosition;
+    int playerControllerPosition;
 
-    //void Start()
-    //{
-    //    platformPos.transform.position = Vector3.zero;
-    //    Instantiate(platforms, platformPos, transform.rotation);
-    //}
+    void Start()
+    {
+        platformPos.transform.position = Vector3.zero;
+        Instantiate(platforms[], platformPos, transform.rotation);
+    }
 
-    //void Update()
-    //{
-    //    if (playerPosition = PlayerControllerPosition + 10)
-    //    {
+    void Update()
+    {
+        if (playerPosition = playerControllerPosition + 10)
+        {
 
-    //        for (int i = 0; i < positions.Length; i++)
-    //        {
-    //            platformPos.transform.position = new Vector3(typeOfPlatform, transform.position.y + 10, 0);
-    //            int typeOfPlatform = Random.Range(0, 100);
-    //            //si el valor random que se genera es igual a uno que se encuentre dentro de las
-    //            //probabilidades se genera el tipo de plataforma correspondiente
-    //            //aqui iría el switch case
+            for (int i = 0; i < positions.Length; i++)
+            {
+                int typeOfPlatform = Random.Range(0, 100);
+                platformPos.transform.position = new Vector3(typeOfPlatform, transform.position.y + 10, 0);
+                
+                //si el valor random que se genera es igual a uno que se encuentre dentro de las
+                //probabilidades se genera el tipo de plataforma correspondiente
+                //aqui iría el switch case
 
-    //            int hasPlatform = Random.Range(0, 100);
-    //            if (hasPlatform <= 10)
-    //            {
-    //                //Instantiate platforms[/*tipo de plataforma*/] posiciones[i]
-    //                //si la probabilidad es verdadera se genera el tipo de plataforma 
-    //            }
+                int hasPlatform = Random.Range(0, 100);
+                if (hasPlatform <= 10)
+                {
+                    Instantiate(platforms[1], positions[i]);
+                    //si la probabilidad es verdadera se genera el tipo de plataforma 
+                }
 
-    //            switch (hasPlatform)
-    //            {
-    //                case: hasPlatform < 20
-    //                {
-    //                        //to do:
-    //                        //instanciamos la plataforma 
-    //                        platformPos = Instantiate(platforms, new Vector3(0, , 0), transform.rotation);
-    //                }
-    //                break;
+                switch (hasPlatform)
+                {
+                    case 1:
+                        hasPlatform < 20
+                        {
+                            //to do:
+                            //instanciamos la plataforma 
+                            Instantiate(platforms[1], positions[i]);
+                            platformPos = Instantiate(platforms, new Vector3(0, , 0), transform.rotation);
+                        }
+                        break;
 
-    //                case: hasPlatform > 20 && < 40
-    //                {
-    //                        //to do:
-    //                        //instanciamos la plataforma 
-    //                        Instantiate(platforms, new Vector3(2.5f,  , 0), transform.rotation);
-    //                }
-    //                break;
-    //            }
+                    case 2:
+                        hasPlatform > 20 && < 40
+                        {
+                            //to do:
+                            //instanciamos la plataforma 
+                            Instantiate(platforms, new Vector3(2.5f,  , 0), transform.rotation);
+                        }
+                        break;
+                }
 
-    //        }
-    //    }
-    //}
+            }
+        }
+    }
 
 }
