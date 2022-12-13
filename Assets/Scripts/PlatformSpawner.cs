673f318fa28c21211d797e4b104ce376782fb288
JessicaERO <jerodriguezo@esimurcia.com>
2022-12-14 00:56:02 +0100

    Avance en el sistema de plataformas
    Creo que he descubierto una forma de que se spawneen las plataformas

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -10,59 +10,73 @@ public class PlatformSpawner : MonoBehaviour
 
     GameObject platformPos;
     GameObject playerPosition;
-    int playerControllerPosition;
+    GameObject playerControllerPosition;
+
+    //Ir al PlayerController
 
     void Start()
     {
         platformPos.transform.position = Vector3.zero;
-        Instantiate(platforms[], platformPos, transform.rotation);
+        
+        for (int i = 0; i < positions.Length; i++)
+        {
+            Instantiate(platforms[0], positions[i].position, transform.rotation);
+        }
     }
 
     void Update()
     {
-        if (playerPosition = playerControllerPosition + 10)
+         //al principio estaba todo aqui
+    }
+
+    public void RandomPlatform()
+    {
+        for (int i = 0; i < positions.Length; i++)
         {
+            int typeOfPlatform = Random.Range(0, 100);
+            platformPos.transform.position = new Vector3(typeOfPlatform, transform.position.y + 10, 0);
 
-            for (int i = 0; i < positions.Length; i++)
+            //si el valor random que se genera es igual a uno que se encuentre dentro de las
+            //probabilidades se genera el tipo de plataforma correspondiente
+            //aqui iría el switch case
+
+            int hasPlatform = Random.Range(0, 100);
+            if (hasPlatform <= 10)
             {
-                int typeOfPlatform = Random.Range(0, 100);
-                platformPos.transform.position = new Vector3(typeOfPlatform, transform.position.y + 10, 0);
-                
-                //si el valor random que se genera es igual a uno que se encuentre dentro de las
-                //probabilidades se genera el tipo de plataforma correspondiente
-                //aqui iría el switch case
-
-                int hasPlatform = Random.Range(0, 100);
-                if (hasPlatform <= 10)
-                {
-                    Instantiate(platforms[1], positions[i]);
-                    //si la probabilidad es verdadera se genera el tipo de plataforma 
-                }
-
-                switch (hasPlatform)
-                {
-                    case 1:
-                        hasPlatform < 20
-                        {
-                            //to do:
-                            //instanciamos la plataforma 
-                            Instantiate(platforms[1], positions[i]);
-                            platformPos = Instantiate(platforms, new Vector3(0, , 0), transform.rotation);
-                        }
-                        break;
-
-                    case 2:
-                        hasPlatform > 20 && < 40
-                        {
-                            //to do:
-                            //instanciamos la plataforma 
-                            Instantiate(platforms, new Vector3(2.5f,  , 0), transform.rotation);
-                        }
-                        break;
-                }
-
+                Instantiate(platforms[1], positions[i]);
+                //si la probabilidad es verdadera se genera el tipo de plataforma 
             }
+
+            //switch (hasPlatform)
+            //{
+            //    case 1:
+            //        hasPlatform < 20
+            //            {
+            //            //to do:
+            //            //instanciamos la plataforma 
+            //            Instantiate(platforms[1], positions[i]);
+            //            platformPos = Instantiate(platforms, new Vector3(0, , 0), transform.rotation);
+            //        }
+            //        break;
+
+            //    case 2:
+            //        hasPlatform > 20 && < 40
+            //            {
+            //            //to do:
+            //            //instanciamos la plataforma 
+            //            Instantiate(platforms, new Vector3(2.5f,  , 0), transform.rotation);
+            //        }
+            //        break;
+            //}
+
+            
+
         }
+    }
+
+    public void DestroyPlatforms()
+    {
+        //aqui detecta las plataformas que hay y las borra
     }
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,16 +18,29 @@ public class PlayerController : MonoBehaviour
     public float time, timeMax;
     public bool isDead;
 
+    public Transform[] filas;
+    public Transform filaInicial;
+    public PlatformSpawner platformSpawnerInicial;
+    //public PlatformSpawner platformSpawner;
+    //variable transform que guarde la fila actual
+
+    //el PlatformSpawner debe ser el que está por debajo de la pantalla
+    //si el player esta en la fila 1, sube la fila 0 a la posición 7 + la altura que haya entre filas 
+    //tras hacer esto llama a la función de borrar plataformas y crear plataformas
+
+    //para subirle la dificultad, por cada ciclo que suba, aumenta la dificultad con una variable
+    //el PlatformSpawner se sustituye por el de la fila en la que estaba
+
     private void Awake()
     {
         instance = this;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         theRB = GetComponent<Rigidbody2D>();
-
+        //PlatformSpawner = platformSpawnerInicial
+        //filaActual = filaInicial
     }
 
     private void Update()
@@ -55,6 +68,8 @@ public class PlayerController : MonoBehaviour
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
+        //si la altura es mayor que la de la fila actual, llamo a la funcion destruirPlataformas del PlatformSpawner y a la RandomSpawner
+        //en ese orden
     }
 
     void FixedUpdate()
