a774271d0b0af2574e7523f62d6218b491da5e05
JessicaERO <jerodriguezo@esimurcia.com>
2023-03-03 12:22:57 +0100

    Shop
    Vamos a hacer una buena shop

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -7,6 +7,7 @@ public class Shop : MonoBehaviour
 {
     public Text coins;
     private int coinsAmount = 0;
+    public int articlePrice;
     void Start()
     {
         if (PlayerPrefs.HasKey("CoinsAmount"))
@@ -15,12 +16,31 @@ public class Shop : MonoBehaviour
             Debug.Log(coinsAmount);
             coins.text = coinsAmount.ToString();
         }
-        
+        ////Carga el valor de las monedas guardado en las playerprefs
+        //int monedas = PlayerPrefs.GetInt("Coins", 0);
+        ////Actualiza el texto de las monedas en la tienda
+        //coins.text = "Coins: " + monedas;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PurchaseArticle()
     {
-        
+        //Carga el valor de las monedas guardado en las playerprefs
+        int monedas = PlayerPrefs.GetInt("Coins", 0);
+
+        if (monedas >= articlePrice)
+        {
+            //Resta el costo del artículo al valor de las monedas
+            monedas -= articlePrice;
+            //Guarda el nuevo valor de las monedas en las playerprefs
+            PlayerPrefs.SetInt("Monedas", monedas);
+            //Actualiza el texto de las monedas en la tienda
+            coins.text = "Monedas: " + monedas;
+
+            //TODO: dar al jugador el artículo comprado
+        }
+        else
+        {
+            //TODO: Mostrar mensaje de que el jugador no tiene suficientes monedas para comprar el artículo
+        }
     }
 }
